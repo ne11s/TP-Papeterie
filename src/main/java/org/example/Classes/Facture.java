67d6060587e55ba5dd0ea8cae05448e2893754d1
nel <nelsonsanchez62480@gmail.com>
2025-05-16 16:50:00 +0200

    modif facture

--- src/main/java/org/example/Classes/Facture.java
+++ src/main/java/org/example/Classes/Facture.java
@@ -1,12 +1,18 @@
 package org.example.Classes;
 
 public class Facture {
-    private int ligneParDefault=10;
-    private int facture;
-    private int nbrFacture;
+    private int numfacture;
+    private static int nbrFacture;
     private String client;
     private String date;
     private double total;
-    
 
+    public Facture(String client, String date) {
+        this.client = client;
+        this.date = date;
+        this.numfacture = ++nbrFacture;
+    }
+    public void ajouterligne(){
+
+    }
 }
